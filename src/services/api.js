b3298aaee2ac0187c9ea0ac1a322f6c9443a3b08
Gabriel Brasil <gabrielbrasil87@outlook.com>
2025-12-03 11:10:56 -0400

    update
    ajustes de conexão com o backend, pós deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://newback-3-2e11.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
